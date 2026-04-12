bddfe668006db5bed244455dde9450e55700b503
Michael C. Mahoney <69471299+mcmahoney@users.noreply.github.com>
2026-04-12 14:35:51 -0700

    Delete _publications/Mahoneyetal2018.m

--- _publications/Mahoneyetal2018.m
+++
@@ -1,7 +0,0 @@
----
-collection: publications
-category: manuscripts
-permalink: /publication/Mahoneyetal2018
-paperurl: 'https://ornneo.journals.publicknowledgeproject.org/index.php/ornneo/article/download/334/ON%2029%20%282018%29%20247-254.pdf'
-citation: 'Michael C Mahoney, Luke Browne, Zoe Diaz-Martin, Jorge Olivo, Jacqueline Cabrera, Monica Gonzalez, Jenny Hazelhurst, Jordan Karubian (2018). Fruit removal by large avian frugivores varies in relation to habitat quality in continuous Neotropical rainforest. Ornitological Neotropical, 247-254.'
----
